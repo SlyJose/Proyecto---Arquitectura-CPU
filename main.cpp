2e158f57c24dd6aedf0e1c16fcde700a450ef354
SlyJose <jpurena14@hotmail.com>
2015-04-28 11:45:03 -0600

    Segundo commit de prueba para QT

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication a(argc, argv);  /* GIt HUB PRUEBA */
 
     QCoreApplication::setOrganizationName("Manchis");
     QCoreApplication::setApplicationName("Qt-SmallTextEditor");
