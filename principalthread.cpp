a7c93f253976c1d292f0281c278521344075d394
SlyJose <jpurena14@hotmail.com>
2015-05-01 18:45:13 -0600

    Se resuelve el problema con el vector de instrucciones.

--- principalthread.cpp
+++ principalthread.cpp
@@ -8,7 +8,7 @@ principalThread::principalThread(QString programa)
     qDebug()<<"El tamano original es "<<fileSize;
     QString::iterator it;
     QString strTemp;
-    int tamVec = 0;
+    int tamVec = 1;
     //REVISAR!!
     for(it = programa.begin(); it!=programa.end(); ++it){
         if(*it==' ' || *it=='\n'){
@@ -17,31 +17,34 @@ principalThread::principalThread(QString programa)
             ++tamVec;
         }
         strTemp.append(*it);
-    }
+    }    
     qDebug()<<strTemp;
     qDebug()<<"El tamano del vector seria de "<<tamVec;
 
     vecInstrucciones = new int[tamVec];
     QString numTemp;
     int j = 0;
-    bool ok;
-    for(it = strTemp.begin(); it!=strTemp.end(); ++it){
-       if(*it=='|'){
-           ++it;
-           vecInstrucciones[j] = numTemp.toInt(&ok, 10);
-           ++j;
-           numTemp.clear();
-       }
-           numTemp.append(*it);
+    int contador = 0;
 
+    for(it = strTemp.begin(); it!=strTemp.end(); ++it){
+        if(*it == '|'){
+            ++j;
+            contador = 0;
+        }else{
+            if(contador == 0){
+                vecInstrucciones[j] = it->digitValue();
+                ++contador;
+            }else{
+                vecInstrucciones[j] = (vecInstrucciones[j] * 10) + it->digitValue();
+                contador = 0;
+            }
+        }
     }
 
     qDebug()<<"El vector quedo como:";
     for(int p=0; p<tamVec; ++p){
         qDebug()<<'-'<<vecInstrucciones[p];
     }
-
-    //**HAY UN PROBLEMA, OMITE EL ULTIMO ELEMENTO DEL STRING Y NO SE PORQUE**
 
 }
 
